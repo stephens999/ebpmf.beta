8cb397e1600dd51248ee37bd4064e14a1d89259d
Matthew Stephens <stephens999@gmail.com>
2023-10-16 09:16:47 -0500

    export ebpmf_obj

--- R/ebpmf_obj.R
+++ R/ebpmf_obj.R
@@ -1,8 +1,12 @@
+#' @title ebpmf_obj
+#' @param fit a ebpmf fit
+#' @details returns the objective function for the current fit. Note that
+#' the objective function for SVD-based and flash-based fits may not be comparable.
+#' @export
 ebpmf_obj = function(fit){
   return(sum(fit$X * fit$M - exp(fit$M+fit$V/2) +
         0.5*(log(2*pi*fit$V)+1)) +   #the relative entropy of Normal with variance V
     +fit$elbo)
-  #+ sum(dnorm(fit$M-fit$B,mean=0,sd=sqrt(1/fit$tau),log=TRUE))
-  #- sum(fit$V*fit$tau/2))
-  #last line is only for svd; done while debugging..., should use elbo if using flash
 }
+
+
